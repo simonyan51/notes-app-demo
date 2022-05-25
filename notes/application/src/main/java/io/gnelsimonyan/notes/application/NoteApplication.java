4c78dd4c7c91fe702df5f0d468242e306e95a01d
simonyan51 <gnelsimonyan51@gmail.com>
2022-05-25 10:35:56 +0400

    build: generated maven projects for clean architecture layers

---
+++ notes/application/src/main/java/io/gnelsimonyan/notes/application/NoteApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Created by Gnel Simonyan
+ * Created on 25/05/2022 10:29
+ *
+ * Copyright (c) 2022 PicsArt.Inc
+ */
+
+package io.gnelsimonyan.notes.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteApplication.class, args);
+    }
+
+}
